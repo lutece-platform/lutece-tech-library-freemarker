cd481e7bd08c03f2ad968face9435317fa4e4329
vanhemsj <jbvanhems@gmail.com>
2026-03-10 10:52:32 +0100

    LUT-32122 : enable HTML auto-escaping by default to prevent XSS

--- src/java/fr/paris/lutece/portal/service/template/AbstractFreeMarkerTemplateService.java
+++ src/java/fr/paris/lutece/portal/service/template/AbstractFreeMarkerTemplateService.java
@@ -52,6 +52,7 @@ import freemarker.cache.FileTemplateLoader;
 import freemarker.cache.MultiTemplateLoader;
 import freemarker.cache.StringTemplateLoader;
 import freemarker.cache.TemplateLoader;
+import freemarker.core.HTMLOutputFormat;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
@@ -322,6 +323,10 @@ public abstract class AbstractFreeMarkerTemplateService implements IFreeMarkerTe
 
         // disable the localized look-up process to find a template
         cfg.setLocalizedLookup( false );
+
+        // enable HTML auto-escaping by default to prevent XSS vulnerabilities
+        cfg.setOutputFormat( HTMLOutputFormat.INSTANCE );
+        cfg.setAutoEscapingPolicy( Configuration.ENABLE_IF_DEFAULT_AUTO_ESCAPING_POLICY );
 
         // keep control localized number formating (can cause pb on ids, and we don't want to use the ?c directive all the time)
         cfg.setNumberFormat( NUMBER_FORMAT_PATTERN );
