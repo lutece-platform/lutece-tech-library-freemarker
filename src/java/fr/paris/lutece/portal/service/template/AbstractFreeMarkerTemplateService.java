0f87f1166744b7f7528352ac40aa6a3805afaebe
evdngsl <158455076+evdngsl@users.noreply.github.com>
2026-03-16 16:29:18 +0100

    LUT-32145 : Allow configuration of auto-escaping default value

--- src/java/fr/paris/lutece/portal/service/template/AbstractFreeMarkerTemplateService.java
+++ src/java/fr/paris/lutece/portal/service/template/AbstractFreeMarkerTemplateService.java
@@ -81,6 +81,7 @@ public abstract class AbstractFreeMarkerTemplateService implements IFreeMarkerTe
     private String _strDefaultPath;
     private int _nTemplateUpdateDelay;
     private boolean _bAcceptIncompatibleImprovements;
+    private boolean _bTemplateAutoEscape;
     private ServletContext _context;
 
     /**
@@ -91,6 +92,16 @@ public abstract class AbstractFreeMarkerTemplateService implements IFreeMarkerTe
     {
         _nTemplateUpdateDelay = nTemplateUpdateDelay;
     }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void setTemplateAutoEscape( boolean bTemplateAutoEscape )
+    {
+        _bTemplateAutoEscape = bTemplateAutoEscape;
+    }
+
     /**
      * {@inheritDoc}
      */
@@ -341,9 +352,12 @@ public abstract class AbstractFreeMarkerTemplateService implements IFreeMarkerTe
         // disable the localized look-up process to find a template
         cfg.setLocalizedLookup( false );
 
-        // enable HTML auto-escaping by default to prevent XSS vulnerabilities
-        cfg.setOutputFormat( HTMLOutputFormat.INSTANCE );
-        cfg.setAutoEscapingPolicy( Configuration.ENABLE_IF_DEFAULT_AUTO_ESCAPING_POLICY );
+        // enable HTML auto-escaping to prevent XSS vulnerabilities
+        if ( _bTemplateAutoEscape )
+        {
+            cfg.setOutputFormat( HTMLOutputFormat.INSTANCE );
+            cfg.setAutoEscapingPolicy( Configuration.ENABLE_IF_DEFAULT_AUTO_ESCAPING_POLICY );
+        }
 
         // keep control localized number formating (can cause pb on ids, and we don't want to use the ?c directive all the time)
         cfg.setNumberFormat( NUMBER_FORMAT_PATTERN );
