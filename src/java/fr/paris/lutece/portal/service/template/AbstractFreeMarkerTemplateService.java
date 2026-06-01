2215af3ef18f3d3b09fb7b5564a50d4b716e7e4c
Jean-Baptiste Vanhems <jean-baptiste.vanhems@paris.fr>
2026-06-01 12:33:09 +0200

    LUT-32603 : Restrict FreeMarker class resolver (SAFER_RESOLVER) to prevent SSTI/RCE via ?new

--- src/java/fr/paris/lutece/portal/service/template/AbstractFreeMarkerTemplateService.java
+++ src/java/fr/paris/lutece/portal/service/template/AbstractFreeMarkerTemplateService.java
@@ -54,6 +54,7 @@ import freemarker.cache.StringTemplateLoader;
 import freemarker.cache.TemplateLoader;
 import freemarker.ext.jakarta.servlet.WebappTemplateLoader;
 import freemarker.core.HTMLOutputFormat;
+import freemarker.core.TemplateClassResolver;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
@@ -328,6 +329,7 @@ public abstract class AbstractFreeMarkerTemplateService implements IFreeMarkerTe
     {
         Version version = ( _bAcceptIncompatibleImprovements ) ? Configuration.VERSION_2_3_28 : Configuration.VERSION_2_3_0;
         Configuration cfg =  new Configuration( version );
+        cfg.setNewBuiltinClassResolver( TemplateClassResolver.SAFER_RESOLVER );
 
         // add core and plugin auto-includes such as macros
         for ( String strFileName : _listPluginsAutoIncludes )
